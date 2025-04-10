7c9f42514d140a634e3c5b20e63d3884e8fa13f7
Alex Vinichenko <allvin.dp@gmail.com>
2025-04-10 21:00:39 +0300

    lesson 20

---
+++ L20/main.js
@@ -0,0 +1,7 @@
+console.log(typeof undefined);
+let strData = 'SomeText';
+let strData2 = 'SomeText2';
+const logText = `I found ${strData2}, and add some 
+
+text here ${strData2}`;
+console.log(logText);
