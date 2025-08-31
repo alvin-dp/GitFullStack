f831ba97655b760748f4a458902a55843fe0926f
Alex Vinichenko <allvin.dp@gmail.com>
2025-09-01 01:41:22 +0300

    hw #45 clear comments

--- L45/new-app/src/redux/reducerRTK.js
+++ L45/new-app/src/redux/reducerRTK.js
@@ -35,28 +35,3 @@ const counterSlice = createSlice({
 export const { increment_home_RTK, decrement_home_RTK,increment_about_RTK,decrement_about_RTK } = counterSlice.actions;
 
 export const counterReducerRTK = counterSlice.reducer;
-
-
-// import { types } from "./actions";
-
-// const initialState = {countHome:0, countAbout:0}
-
-// function counterReducer (state=initialState,action)
-// {
-//     console.log(action.type,state);
-//     switch (action.type) {
-//         case types.ACTION_INCREMENT:           
-//             return { ...state, countHome: state.countHome + action.payload}
-//         case types.ACTION_DECREMENT:          
-//             return { ...state, countHome: state.countHome - action.payload}  
-//         case types.ACTION_INCREMENT_ABOUT:          
-//             return { ...state, countAbout: state.countAbout + action.payload}   
-//         case types.ACTION_DECREMENT_ABOUT:          
-//             return { ...state, countAbout: state.countAbout - action.payload}                            
-//         default:
-//             return state;
-//     }
-// }
-
-
-// export {counterReducer}
